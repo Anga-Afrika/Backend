b4e57cfa2040a023c576a67af252e5ab2855ee54
vivian347 <vkinyua001@gmail.com>
2024-03-11 20:47:39 +0300

    email conf

--- backend/settings.py
+++ backend/settings.py
@@ -121,6 +121,13 @@ DATABASES = {
 #     }
 # }
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'johndoebosco@gmail.com'
+EMAIL_HOST_PASSWORD = 'ogcn etcn ruiq wkvn'
+EMAIL_USE_TLS = True
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
