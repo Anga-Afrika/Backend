2b311405fea8fc56a45a0feef0796379968afe36
NJIHIA <harunnjihia455@gmail.com>
2024-03-19 16:47:43 +0300

    :bug: CORS CORS CORS

--- backend/settings.py
+++ backend/settings.py
@@ -102,7 +102,7 @@ WSGI_APPLICATION = "backend.wsgi.application"
 database_url = os.environ.get('DATABASE_URL')
 
 # Parse the DATABASE_URL using dj_database_url
-db_config = dj_database_url.parse("postgres://ghala_user:NAsQ5zaqowoiiRvUZhCNHJQG4fyD8FDU@dpg-cnsongta73kc73b62r70-a.oregon-postgres.render.com/ghala")
+db_config = dj_database_url.parse("postgres://ghala_user:NAsQ5zaqowoiiRvUZhCNHJQG4fyD8FDU@dpg-cnsongta73kc73b62r70-a/ghala")
 
 # Add SSL disable option to the OPTIONS dictionary
 db_config['OPTIONS'] = {'sslmode': 'require'}
@@ -209,7 +209,8 @@ SIMPLE_JWT = {
 }
 
 CORS_ALLOWED_ORIGINS = [
-    'http://127.0.0.0.1',   # all urls allowed
+    'http://127.0.0.0.1',
+    'localhost'   # all urls allowed
 ]
 
 CORS_ALLOW_CREDENTIALS = True
