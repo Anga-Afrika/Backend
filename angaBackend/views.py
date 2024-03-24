5be7f99a4099581d6a79a0c99ca0f8d0e597d191
NJIHIA <harunnjihia455@gmail.com>
2024-03-24 18:37:26 +0300

    twilio-integration

--- angaBackend/views.py
+++ angaBackend/views.py
@@ -37,10 +37,50 @@ def create_devices(request, device_id = None):
 
 @api_view(['POST'])
 def get_data(request):
-    #call the get_measurements function
+    # Call the get_measurements function
     query = request.data.get('query')
     device_data = get_measurements(query)
+
+    # Check if thresholds are exceeded
+    try:
+        threshold = Threshold.objects.first()
+    except Threshold.DoesNotExist:
+        # If Threshold object doesn't exist, return device data without checking thresholds
+        return JsonResponse(device_data, safe=False)
+
+    temperature_threshold = threshold.temperature_threshold
+    humidity_threshold = threshold.humidity_threshold
+
+    temperature = device_data.get('temperature')
+    humidity = device_data.get('humidity')
+
+    if temperature is not None and temperature > temperature_threshold:
+        send_twilio_message("Temperature threshold exceeded!")
+    if humidity is not None and humidity > humidity_threshold:
+        send_twilio_message("Humidity threshold exceeded!")
+
     return JsonResponse(device_data, safe=False)
+
+
+# Function to send Twilio message
+def send_twilio_message(message):
+    # Your Twilio credentials
+    account_sid = 'ACc3302715d7eb7b3742b84f90165cb131'
+    auth_token = 'afcce87a9581ddda5ae856f410ea73ee'
+    twilio_number = '+16562193403'
+    
+    # Recipient's phone number
+    recipient_number = '+254792386531'
+
+    # Initialize Twilio client
+    client = Client(account_sid, auth_token)
+
+    # Send message
+    message = client.messages.create(
+        body=message, 
+        from_=twilio_number, 
+        to=recipient_number
+    )
 
 @api_view(['GET'])
 def get_weekly_temp_average(request):
